342d4faca8aad4e4ee976a535277f736f600222f
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-07-07 17:19:14 +0700

    update pyrogram/methods/chats/promote_chat_member.py

--- pyrogram/methods/chats/promote_chat_member.py
+++ pyrogram/methods/chats/promote_chat_member.py
@@ -17,7 +17,7 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrofork.  If not, see <http://www.gnu.org/licenses/>.
 
-from typing import Union
+from typing import Union, Optional
 
 import pyrogram
 from pyrogram import raw, types, errors
