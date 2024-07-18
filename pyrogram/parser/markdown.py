d636c16eb178d1b2edb83e3afc77f49acf8e8161
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-07-18 15:55:15 +0700

    update pyrogram/parser/markdown.py

--- pyrogram/parser/markdown.py
+++ pyrogram/parser/markdown.py
@@ -17,6 +17,7 @@
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
 import html
+import logging
 import re
 from typing import Optional
 
@@ -32,6 +33,8 @@ STRIKE_DELIM = "~~"
 SPOILER_DELIM = "||"
 CODE_DELIM = "`"
 PRE_DELIM = "```"
+BLOCKQUOTE_DELIM = ">"
+BLOCKQUOTE_EXPANDABLE_DELIM = "**>"
 
 MARKDOWN_RE = re.compile(r"({d})|\[(.+?)\]\((.+?)\)".format(
     d="|".join(
@@ -53,18 +56,63 @@ OPENING_TAG = "<{}>"
 CLOSING_TAG = "</{}>"
 URL_MARKUP = '<a href="{}">{}</a>'
 FIXED_WIDTH_DELIMS = [CODE_DELIM, PRE_DELIM]
+CODE_TAG_RE = re.compile(r"<code>.*?</code>")
 
 
 class Markdown:
     def __init__(self, client: Optional["pyrogram.Client"]):
         self.html = HTML(client)
 
+    def blockquote_parser(self, text):
+        text = re.sub(r'\n&gt;', '\n>', re.sub(r'^&gt;', '>', text))
+        lines = text.split('\n')
+        result = []
+
+        in_blockquote = False
+
+        for line in lines:
+            if line.startswith(BLOCKQUOTE_DELIM):
+                if not in_blockquote:
+                    line = re.sub(r'^> ', OPENING_TAG.format("blockquote"), line)
+                    line = re.sub(r'^>', OPENING_TAG.format("blockquote"), line)
+                    in_blockquote = True
+                    result.append(line.strip())
+                else:
+                    result.append(line[1:].strip())
+            elif line.startswith(BLOCKQUOTE_EXPANDABLE_DELIM):
+                if not in_blockquote:
+                    line = re.sub(r'^\*\*> ', OPENING_TAG.format("blockquote expandable"), line)
+                    line = re.sub(r'^\*\*>', OPENING_TAG.format("blockquote expandable"), line)
+                    in_blockquote = True
+                    result.append(line.strip())
+                else:
+                    result.append(line[3:].strip())
+            else:
+                if in_blockquote:
+                    line = CLOSING_TAG.format("blockquote") + line
+                    in_blockquote = False
+                result.append(line)
+
+        if in_blockquote:
+            line = result[len(result)-1] + CLOSING_TAG.format("blockquote")
+            result.pop(len(result)-1)
+            result.append(line)
+
+        return '\n'.join(result)
+
     async def parse(self, text: str, strict: bool = False):
         if strict:
             text = html.escape(text)
+        text = self.blockquote_parser(text)
 
         delims = set()
         is_fixed_width = False
+
+        placeholders = {}
+        for i, code_section in enumerate(CODE_TAG_RE.findall(text)):
+            placeholder = f"{{CODE_SECTION_{i}}}"
+            placeholders[placeholder] = code_section
+            text = text.replace(code_section, placeholder, 1)
 
         for i, match in enumerate(re.finditer(MARKDOWN_RE, text)):
             start, _ = match.span()
@@ -113,6 +161,9 @@ class Markdown:
 
             text = utils.replace_once(text, delim, tag, start)
 
+        for placeholder, code_section in placeholders.items():
+            text = text.replace(placeholder, code_section)
+
         return await self.html.parse(text)
 
     @staticmethod
@@ -141,7 +192,7 @@ class Markdown:
                 start_tag = f"{PRE_DELIM}{language}\n"
                 end_tag = f"\n{PRE_DELIM}"
             elif entity_type == MessageEntityType.BLOCKQUOTE:
-                start_tag = end_tag = PRE_DELIM
+                start_tag = BLOCKQUOTE_DELIM + " "
             elif entity_type == MessageEntityType.SPOILER:
                 start_tag = end_tag = SPOILER_DELIM
             elif entity_type == MessageEntityType.TEXT_LINK:
